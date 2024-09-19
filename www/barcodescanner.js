04959e299957ec675188da34aa3abc5acf987693
Florian Pechwitz <florian.pechwitz@simplifier.io>
2024-09-19 16:09:15 +0200

    - Remove unused barcode formats from the `format` object in `barcodescanner.js`

--- www/barcodescanner.js
+++ www/barcodescanner.js
@@ -39,7 +39,6 @@
      * @type Object
      */
     this.format = {
-        "all_1D": 61918,
         "aztec": 1,
         "codabar": 2,
         "code_128": 16,
@@ -49,16 +48,10 @@
         "ean_13": 128,
         "ean_8": 64,
         "itf": 256,
-        "maxicode": 512,
-        "msi": 131072,
         "pdf_417": 1024,
-        "plessey": 262144,
         "qr_CODE": 2048,
-        "rss_14": 4096,
-        "rss_EXPANDED": 8192,
         "upc_A": 16384,
-        "upc_E": 32768,
-        "upc_EAN_EXTENSION": 65536
+        "upc_E": 32768
         };
   }
 
